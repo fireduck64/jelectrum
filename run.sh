968dc8ffcf5c2be43ea03b00630daf85cbf16215
fireduck64 <fireduck@gmail.com>
2017-09-19 12:07:44 -0700

    Adding example run file

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Makes a copy of the jar file so the jar file can be recompiled
+# during runtime without problems
+
+mkdir -p run
+rsync -aq jar/Jelectrum.jar run/Jelectrum-run.jar
+java -Xmx2g -jar run/Jelectrum-run.jar jelly.conf
+
+
